5fb551aad6d2ed1050c3a1db48d04198357f4cd3
Jascha Haldemann <jh@oxon.ch>
2021-04-24 18:11:35 +0200

    some bug fixes

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,7 +5,7 @@ class Member < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :card_id, uniqueness: true
+  validates :card_id, uniqueness: true, allow_blank: true
 
   def new?
     (!self.active || !self.card_id.present?) && self.created_at.today?
