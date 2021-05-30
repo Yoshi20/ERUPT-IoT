56015c6901430520384e76e5a4381d1055ef8bae
Jascha Haldemann <jh@oxon.ch>
2021-05-30 16:46:02 +0200

    action cable production configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'ws://erupt-iot.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://erupt-iot.herokuapp.com', /http:\/\/erupt-iot.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
