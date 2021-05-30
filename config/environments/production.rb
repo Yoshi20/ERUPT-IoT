35c0566a5e68f46b3d97dacc608adde246c7cc2f
Jascha Haldemann <jh@oxon.ch>
2021-05-30 16:56:02 +0200

    action_cable.disable_request_forgery_protection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'ws://erupt-iot.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'http://erupt-iot.herokuapp.com', /http:\/\/erupt-iot.herokuapp.*/ ]
+  config.action_cable.disable_request_forgery_protection = true #blup: risky?
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
