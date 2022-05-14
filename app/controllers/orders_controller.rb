8664f9a63524687913a1343b1e0bf8a016062ea8
Jascha Haldemann <jh@oxon.ch>
2022-05-14 15:04:41 +0200

    paginate offers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.closed
+    @orders = Order.closed.paginate(page: params[:page], per_page: Order::MAX_ORDERS_PER_PAGE)
   end
 
   # GET /orders_fullscreen
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,8 @@ class Order < ApplicationRecord
   scope :open, -> { where(acknowledged: false) }
   scope :closed, -> { where(acknowledged: true).order(acknowledged_at: :desc) }
 
+  MAX_ORDERS_PER_PAGE = 100
+
   def new?
     self.acknowledged_at > 5.minute.ago
   end
