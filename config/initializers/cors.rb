cb9a3a0dd55a59b5174e3dadf5f4f7853e1b8c2b
Jascha Haldemann <jh@oxon.ch>
2023-02-12 15:32:06 +0100

    origins '*'

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://iframetester.com:443'
-    resource '/games_for_iframe', headers: :any, methods: [:get]
-    # resource '/feedbacks/extern', headers: :any, methods: [:get]
-    # resource '/members/extern', headers: :any, methods: [:get]
-  end
-
-  allow do
-    origins 'https://www.erupt.ch:443'
+    origins '*'
     resource '/games_for_iframe', headers: :any, methods: [:get]
     # resource '/feedbacks/extern', headers: :any, methods: [:get]
     # resource '/members/extern', headers: :any, methods: [:get]
